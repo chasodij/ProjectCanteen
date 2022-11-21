5725f5b1b34fe3badfc38e5f972cf320b3bf537a
Ярослав Масенко <chasodij@outlook.com>
2022-11-21 13:27:02 +0200

    Make User's LastName nullable

--- ProjectCanteen.DAL/Entities/User.cs
+++ ProjectCanteen.DAL/Entities/User.cs
@@ -5,7 +5,7 @@ namespace ProjectCanteen.DAL.Entities
     public class User : IdentityUser
     {
         public string FirstName { get; set; } = String.Empty;
-        public string LastName { get; set; } = String.Empty;
+        public string? LastName { get; set; }
         public string? Patronymic { get; set; }
     }
 }
